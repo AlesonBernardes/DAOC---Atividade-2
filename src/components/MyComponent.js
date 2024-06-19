3c5d5b3409c562255253232082b309f6a2743483
AlesonBernardes <166345319+AlesonBernardes@users.noreply.github.com>
2024-06-19 10:45:24 -0300

    primeiro commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,19 @@
+// src/components/MyComponent.js
+
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>Meu Componente</h2>
+      <p>Este é um componente simples em React.</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+       
+      </ul>
+    </div>
+  );
+}
+
+export default MyComponent;
